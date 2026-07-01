d514759142496a2d17ff15eb6b4877f0d5e936a9
Ксения Будкина <obscure.74@yandex.ru>
2026-07-01 19:01:41 +0300

    Fix: исправлен импорт в api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,6 +10,4 @@ router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
 ]
